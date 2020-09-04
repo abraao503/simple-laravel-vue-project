d223e3065a7d89fe9ff22bfef599550a1efb2526
abraao503 <abraao503@gmail.com>
2020-09-04 09:45:04 -0400

    add route validation

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\ModelRepository;
 
 class RepositoryController extends Controller
 {
-
   private $objRepository;
 
   public function __construct()
@@ -17,22 +17,53 @@ class RepositoryController extends Controller
 
   public function index()
   {
-    $repositories = $this->objRepository->all()->toJson(JSON_PRETTY_PRINT);
-
-    return response($repositories, 200);
+    try {
+      $repositories = $this->objRepository->all()->toJson(JSON_PRETTY_PRINT);
+      return response($repositories, 200);
+    } catch(\Throwable $e) {
+      return response([], 500);
+    }
   }
 
   public function store(Request $request)
   {
-    $this->objRepository->create([
-      'name'=>$request->name,
-      'description'=>$request->description,
-      'url'=>$request->url
-    ]);
+    $response;
+    $rules = [
+      'name' => 'required|max:255',
+      'description' => 'max:255',
+      'url' => 'required|max:255',
+    ];
+
+    try {
+      $validator = Validator::make($request->all(), $rules);
+      if ($validator->fails()) {
+        $response['errors'] = $validator->messages();
+      }else{
+        $response = $this->objRepository->create([
+          'name'=>$request->name,
+          'description'=>$request->description,
+          'url'=>$request->url
+        ]);
+      }
+
+      return response($response, $response['errors'] ? 400 : 200);
+    } catch(\Throwable $e) {
+      return response([], 500);
+    }
   }
 
   public function destroy($id)
-  {
-    $del = $this->objRepository->destroy($id);
+  { 
+    try{
+      $deletedRepository = $this->objRepository->destroy($id);
+      if($deletedRepository){
+        return response([], 204);
+      } else {
+        $reponse['error'] = 'repository not found';
+        return response($reponse, 404);
+      }
+    } catch(\Throwable $e) {
+      return response([], 500);
+    }
   }
 }
